8ccb0d6212c4e3a8b1db31466876dc75d3248173
“mpallenjr”git config --global user.email “mpallenjnr@gmail.com” <mikeallen@Michaels-MBP.attlocal.net>
2021-11-11 11:13:38 -0600

    create action hardcoded working.

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -3,4 +3,18 @@ class PhotosController < ApplicationController
     all_photos = Photo.all
     render json: all_photos.as_json
   end
+
+  def create
+    create_photo = Photo.new(
+      
+      name: "Happy Sun",
+      price: 55,
+      description: "bask in it's radiance",
+      image_url: "https://images.pexels.com/photos/87611/sun-fireball-solar-flare-sunlight-87611.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+    
+    
+    )
+    create_photo.save
+    render json: create_photo.as_json 
+  end
 end
